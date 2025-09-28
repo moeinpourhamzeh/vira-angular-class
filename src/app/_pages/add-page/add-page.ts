395cbe4dbdefc9a52f37c2200dde9120731221e5
Moein <64212565+moeinpourhamzeh@users.noreply.github.com>
2025-09-28 18:11:41 +0330

    ADD: Service

--- src/app/_pages/add-page/add-page.ts
+++ src/app/_pages/add-page/add-page.ts
@@ -2,6 +2,7 @@ import {Component, inject} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { TodoModel } from '../../_models/todo.model';
 import {TodoService} from '../../_services/todo-service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-add-page',
@@ -12,10 +13,12 @@ import {TodoService} from '../../_services/todo-service';
   styleUrl: './add-page.css'
 })
 export class AddPage {
+  router = inject(Router);
   todoService = inject(TodoService);
   todoForCreate: TodoModel = new TodoModel();
 
   addClicked() {
     this.todoService.add(this.todoForCreate);
+    this.router.navigate(['/list']);
   }
 }
--- src/app/_pages/list-page/list-page.ts
+++ src/app/_pages/list-page/list-page.ts
@@ -1,10 +1,13 @@
 import {Component, inject} from '@angular/core';
 import { TodoModel } from '../../_models/todo.model';
 import {TodoService} from '../../_services/todo-service';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-list-page',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './list-page.html',
   styleUrl: './list-page.css'
 })
