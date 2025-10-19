b32fefe2155d68a827fa4dd32da8c2c95c4288c4
Moein <64212565+moeinpourhamzeh@users.noreply.github.com>
2025-10-19 19:08:06 +0330

    For Sina

--- src/app/_services/todo-service.ts
+++ src/app/_services/todo-service.ts
@@ -1,12 +1,22 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
 import {TodoModel} from '../_models/todo.model';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  http = inject(HttpClient)
   todoList: TodoModel[] = []
 
+
+  getAllTodos() {
+    this.http.get('http://localhost:1337/api/todos')
+  }
+
+
+
+  // I will change them soon
   add(todoForCreate: TodoModel) {
     this.todoList.push(todoForCreate);
   }
